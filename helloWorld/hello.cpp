d280083a51008f536718f8fe942d71558aa8c847
kippingTap <yanshuyu233@gmail.com>
2018-11-19 02:16:07 +0800

    add another hello world echo to std output stream

--- helloWorld/hello.cpp
+++ helloWorld/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout << "hello world" << endl;
+    cout << "another hello world" << endl;
     system("pause");
     return 0;
 }
